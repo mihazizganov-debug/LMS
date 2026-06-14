8501ee130e2de9f4961e5209f4a7553168f9e615
Mikhail Zizganov <mzizganov@yandex.ru>
2026-06-14 20:40:01 +0300

    Сделал: добавил django-filter, обновил настройки и URL

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "users",
     "lms",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ AUTH_USER_MODEL = "users.User"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+REST_FRAMEWORK = {"DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"]}
--- config/urls.py
+++ config/urls.py
@@ -1,11 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.shortcuts import redirect
 from django.urls import include, path
 
 urlpatterns = [
-    path("", lambda request: redirect("api/", permanent=False)),
     path("admin/", admin.site.urls),
     path("api/", include("lms.urls")),
+    path("api/users/", include("users.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
