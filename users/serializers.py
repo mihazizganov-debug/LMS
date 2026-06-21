a77d52e7237e114b64ac0bad14af91844df6daa9
Mikhail Zizganov <mzizganov@yandex.ru>
2026-06-21 22:05:20 +0300

    feat: добавил сериализаторы для CRUD пользователей

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,35 @@
-from rest_framework import serializers
 
 from .models import Payment
+from rest_framework import serializers
+from .models import User
 
 
 class PaymentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Payment
         fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'phone', 'city', 'avatar', 'is_active', 'is_staff']
+        read_only_fields = ['id', 'is_active', 'is_staff']
+
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'password', 'phone', 'city', 'avatar']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            password=validated_data['password'],
+            phone=validated_data.get('phone', ''),
+            city=validated_data.get('city', ''),
+            avatar=validated_data.get('avatar', None),
+        )
+        return user
