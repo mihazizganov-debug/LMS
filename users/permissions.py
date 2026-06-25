f24e67363a047a8dbd1f95f8b803989a4c0df2ef
Mikhail Zizganov <mzizganov@yandex.ru>
2026-06-25 22:31:34 +0300

    Добавил кастомные права доступа IsModerator, IsOwner, IsNotModerator

--- users/permissions.py
+++ users/permissions.py
@@ -2,16 +2,15 @@ from rest_framework import permissions
 
 
 class IsModerator(permissions.BasePermission):
-    """
-    Разрешает доступ только пользователям в группе 'Модератор'
-    """
     def has_permission(self, request, view):
         return request.user.groups.filter(name='Модератор').exists()
 
 
 class IsOwner(permissions.BasePermission):
-    """
-    Разрешает доступ только владельцу объекта
-    """
     def has_object_permission(self, request, view, obj):
         return obj.owner == request.user
+
+
+class IsNotModerator(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return not request.user.groups.filter(name='Модератор').exists()
