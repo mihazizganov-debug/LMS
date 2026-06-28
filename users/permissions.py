7902cf5850307dea33567652ee01d40ccaa2f57b
Mikhail Zizganov <mzizganov@yandex.ru>
2026-06-28 21:29:27 +0300

    Исправил права доступа (PEP8)

--- users/permissions.py
+++ users/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        return request.user.groups.filter(name='Модератор').exists()
+        return request.user.groups.filter(name="Модератор").exists()
 
 
 class IsOwner(permissions.BasePermission):
@@ -13,4 +13,4 @@ class IsOwner(permissions.BasePermission):
 
 class IsNotModerator(permissions.BasePermission):
     def has_permission(self, request, view):
-        return not request.user.groups.filter(name='Модератор').exists()
+        return not request.user.groups.filter(name="Модератор").exists()
